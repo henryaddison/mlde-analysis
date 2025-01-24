9b61c3d085bc5e77f9df261883b0051195de03bb
Henry Addison <henryaddison@users.noreply.github.com>
2025-01-24 10:56:03 +0000

    move frac contrib functions from pers paper notebook to lib

--- src/mlde_analysis/fractional_contribution.py
+++ src/mlde_analysis/fractional_contribution.py
@@ -87,3 +87,58 @@ def plot_fractional_contribution(
     ax.set_xlim([0.1, 200.0])
     # ax.set_yscale("log")
     ax.set_ylim([0, 2])  # for diff or change use ax.set_ylim([-0.4, 0.4])
+
+
+def frac_contrib_change(pr_da):
+    fpr = pr_da.where(pr_da["time_period"] == "future", drop=True)
+    ffraccontrib, fbinvals = compute_fractional_contribution(fpr)
+
+    hpr = pr_da.where(pr_da["time_period"] == "historic", drop=True)
+    hfraccontrib, hbinvals = compute_fractional_contribution(hpr)
+
+    assert np.all(fbinvals == hbinvals)
+
+    return ffraccontrib - hfraccontrib, fbinvals
+
+
+def plot_fractional_contribution_change(frcontrib_change_data, ax, title):
+    for pred in frcontrib_change_data:
+        ax.plot(
+            pred["data"][1][1:],
+            pred["data"][0][1:],
+            # baseline=None,
+            # fill=False,
+            color=pred["color"],
+            alpha=pred.get("alpha", 0.95),
+            linestyle=pred.get("linestyle", "-"),
+            linewidth=1,
+            label=f"{pred['label']}",
+        )
+    ax.set_title(title)
+    ax.set_xlabel("Precip (mm/day)")
+    ax.set_ylabel("Change in frac. contrib.")
+    ax.set_ylim([-0.4, 0.4])
+    ax.set_xscale("log")
+    ax.set_xlim([0.1, 200.0])
+
+    # # linthresh based on minimum value
+    # linthresh = (min((map(lambda h: np.min(np.fabs(h["data"][h["data"].nonzero()])), frcontrib_change_data))))
+    # print(linthresh)
+    # linthresh = (10 ** math.floor(math.log10(linthresh)))/2
+    # print(linthresh)
+
+    # # linthresh based on minimum value from CPM
+    # linthresh = np.min(np.fabs(frcontrib_change_data[0]["data"][frcontrib_change_data[0]["data"].nonzero()]))
+    # print(linthresh)
+    # linthresh = (10 ** math.floor(math.log10(linthresh)))/2
+    # print(linthresh)
+
+    # linthreshold based on single observation at reasonably high precip
+    # mindensity = 1 / (np.product(CPM_DAS["pr"].shape)/3) # divide by 3 as considering single time periods
+    # print(mindensity)
+    # linthresh = 10 ** (math.floor(math.log10(100*mindensity))) / 2 # multiply by 100 as frac contrib is density times intensity
+    # print(linthresh)
+
+    # ax.set_yscale("symlog", linthresh=linthresh)
+    ax.tick_params(axis="both", which="major")
+    # ax.legend(ncols=2, fontsize="small")
