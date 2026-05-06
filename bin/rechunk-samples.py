875c22442c5a1e621c89f154b7a6ce10f85c4164
Henry Addison <henryaddison@users.noreply.github.com>
2026-05-06 11:36:42 +0100

    helper script to rechunk samples created with old chunk sizes

---
+++ bin/rechunk-samples.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+import xarray as xr
+import shutil
+
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def main(
+    filepath: Path,
+):
+
+    filepath = "predictions.zarr"
+    rechunked_filepath = filepath.replace("predictions", "predictions-rechunked")
+
+    typer.echo(f"Rechunking {filepath} to {rechunked_filepath}")
+
+    ds = xr.open_dataset(filepath, chunks={})
+    ds["pr"] = ds["pr"].chunk(
+        {
+            "ensemble_member": 1,
+            "time": 90,
+            "frame": 24,
+            "grid_longitude": 64,
+            "grid_latitude": 64,
+        }
+    )
+
+    ds.to_zarr(rechunked_filepath)
+
+    typer.echo(f"Replacing {filepath} with {rechunked_filepath}")
+    shutil.move(rechunked_filepath, filepath)
+
+
+if __name__ == "__main__":
+    app()
