603ac63b396d481c3c13648490bebb6b91198410
Henry Addison <henryaddison@users.noreply.github.com>
2026-07-30 14:09:11 +0100

    use 0.1 mm/hr as wet/dry threshold

--- src/mlde_analysis/idf.py
+++ src/mlde_analysis/idf.py
@@ -3,7 +3,7 @@ import xarray as xr
 
 # THRESHOLD = 0.01 # threshold value in mm/10min
 # BINS = [0.0,0.01,0.02,0.05,0.1,0.2,0.5,1.0,2.0,5.0,10.0,20.0,50.0,1000.0]
-THRESHOLD = 0.05  # threshold value in mm/hr used to mark end of a spell
+THRESHOLD = 0.1  # threshold value in mm/hr used to mark end of a spell
 INTENSITY_BINS = [0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0, 20.0, 50.0, 1000.0]
 DURATIONS_BINS = [1, 2, 3, 4, 5, 6, 12, 18, 24, 36, 48, 72]  # durations of 1 hour
 
