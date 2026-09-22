da38269eb207733ae04ef7de6ed285daa5047ecb
Henry Addison <henryaddison@users.noreply.github.com>
2026-09-22 21:15:14 +0100

    helpers for precomputing stats on datasets

--- src/mlde_analysis/bin/stats.py
+++ src/mlde_analysis/bin/stats.py
@@ -38,3 +38,15 @@ def predictions(prediction_run_path: Path, eval_vars: list[str]):
     samples_stats = stats.from_dataset(ds, (0, 200), eval_vars).compute()
 
     samples_stats.to_zarr(prediction_run_path / f"eval_stats.zarr", mode="w")
+
+
+@app.command()
+def dataset(dataset_split_path: Path, eval_vars: list[str]):
+    """Store evaluation statistics for a dataset of samples split over simulation ensemble members."""
+
+    logger.info(f"Computing dataset evaluation statistics for {dataset_split_path}...")
+    ds = xr.open_dataset(dataset_split_path / "predictands.zarr", chunks={})
+
+    samples_stats = stats.from_dataset(ds, (0, 200), eval_vars).compute()
+
+    samples_stats.to_zarr(dataset_split_path / f"eval_stats.zarr", mode="w")
